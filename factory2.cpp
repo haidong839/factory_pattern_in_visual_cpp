36e2d4768ade91b9d7f22676798dc5b469cb82ea
hai dong <hai_dong1@yahoo.com>
2014-12-11 11:24:48 +0100

    Update factory2.cpp

--- factory2.cpp
+++ factory2.cpp
@@ -1,8 +1,4 @@
-// factory2.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-#include <iostream>
 
 using namespace std;
 using namespace System;
@@ -10,42 +6,44 @@ using namespace System;
 class IAnimal
 {
 public:
-    virtual int GetNumberOfLegs() const = 0;
-    virtual void Speak() = 0;
-    virtual void Free() = 0;
+	virtual int GetNumberOfLegs() const = 0;
+	virtual void Speak() = 0;
+	virtual void Free() = 0;
 };
 
 // IAnimal implementations
 class Cat : public IAnimal
 {
 public:
-    int GetNumberOfLegs() const { return 4; }
-    void Speak() { cout << "Meow" << endl; }
-    void Free() { delete this; }
+	int GetNumberOfLegs() const { return 4; }
+	void Speak() { Console::Write( "Meow"); }
+	void Free() { delete this; }
 };
 
 class Dog : public IAnimal
 {
 public:
-    int GetNumberOfLegs() const { return 4; }
-    void Speak() { cout << "Woof" << endl; }
-    void Free() { delete this; }
+	int GetNumberOfLegs() const { return 4; }
+	void Speak() { Console::Write( "Woof"); }
+	void Free() { delete this; }
 };
 
 class AnimalFactory
 {
 private:
-   
+
 public:
 	AnimalFactory(){;};
-    ~AnimalFactory() { ; }
-    IAnimal *CreateAnimal(System::String^  animalName);
+	~AnimalFactory() { 		
+		delete this; 
+	}
+	IAnimal *CreateAnimal(System::String^  animalName);
 };
 
 
 IAnimal* AnimalFactory::CreateAnimal(System::String^  animalName)
 {
-    if(!String::Compare((System::String ^)animalName,(System::String ^)"Cat"))
+	if(!String::Compare(animalName,"Cat"))
 		return new Cat();
 	else 
 		return new Dog();
@@ -54,32 +52,37 @@ IAnimal* AnimalFactory::CreateAnimal(System::String^  animalName)
 int main(int argc, _TCHAR* argv[])
 {
 	IAnimal *pAnimal = NULL;
-	AnimalFactory* animalFactory = NULL;
-    System::String^ animalName;
-	animalFactory = new AnimalFactory();
-    while( pAnimal == NULL )
-    {
-        cout << "Type the name of an animal or 'q' to quit: ";
-        animalName = Console::ReadLine();
+	System::String^ animalName;
+	AnimalFactory* animalFactory = new AnimalFactory();
 
-       if(!String::Compare((System::String ^)animalName,(System::String ^)"q"))
-        break;
-		
-        IAnimal *pAnimal = animalFactory->CreateAnimal(animalName);
-        if( pAnimal )
-        {
-            cout << "Your animal has "<< pAnimal->GetNumberOfLegs() << "legs."<< endl;
-            cout << "Your animal says: ";
-            pAnimal->Speak();
-        }
-        else
-        {
-            cout << "That animal doesn’t exist in the farm! Choose another!"<< endl;
-        }
-        if( pAnimal )
-            pAnimal->Free();
-        pAnimal = NULL;
-    }
-    return 0;
+	while( pAnimal == NULL )
+	{
+		Console::WriteLine( "Type the name of an animal, Cat or Dog, or 'q' to quit: ");
+		animalName = Console::ReadLine();
+
+		if(!String::Compare(animalName,"q"))
+		{
+			break;
+		}
+
+		IAnimal *pAnimal = animalFactory->CreateAnimal(animalName);
+		if( pAnimal )
+		{			
+			String^ valueString = ( pAnimal->GetNumberOfLegs()).ToString();
+			Console::WriteLine(  "Your animal has " + valueString + " legs.");			
+			Console::Write("Your animal says: ");
+			pAnimal->Speak();
+			Console::WriteLine();
+		}
+		else
+		{
+			Console::WriteLine( "That animal doesn’t exist in the farm! Choose another!");
+		}
+		if( pAnimal )
+			pAnimal->Free();
+		pAnimal = NULL;
+	}
+
+	return 0;
 }
 
